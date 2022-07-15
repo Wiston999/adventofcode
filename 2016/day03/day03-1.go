8ffd5822e694a1bbcfeeda543ae44f25d0e07197
Victor Cabezas <vcabezas@tuenti.com>
2022-07-15 10:45:10 +0200

    Add day 3

---
+++ 2016/day03/day03-1.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/urfave/cli/v2"
+)
+
+type Triangle struct {
+	A, B, C int
+}
+
+func setLogLevel(level string) {
+	switch strings.ToLower(level) {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.Fatal(fmt.Sprintf("Unkown log level %s", level))
+	}
+}
+
+func echo(msg, f string) {
+	var file *os.File
+	if f == "-" {
+		file = os.Stdout
+	} else {
+		var err error
+		file, err = os.OpenFile(f, os.O_RDWR|os.O_CREATE, 0644)
+		if err != nil {
+			log.Error(fmt.Sprintf("Error opening file %s for writing output: %v", f, err))
+			return
+		}
+		defer file.Close()
+	}
+	fmt.Fprintf(file, msg)
+}
+
+func parseInput(input string) (data []Triangle, err error) {
+	tmpData, err := os.ReadFile(input)
+	if err != nil {
+		log.Error(fmt.Sprintf("Error opening file %s for reading input: %v", input, err))
+		return
+	}
+	regex := *regexp.MustCompile(`\s+(\d+)\s+(\d+)\s+(\d+)`)
+	res := regex.FindAllStringSubmatch(string(tmpData), -1)
+	for i := range res {
+		a, _ := strconv.Atoi(string(res[i][1]))
+		b, _ := strconv.Atoi(string(res[i][2]))
+		c, _ := strconv.Atoi(string(res[i][3]))
+		triangle := Triangle{a, b, c}
+		data = append(data, triangle)
+	}
+	return
+}
+
+func isValid(t Triangle) bool {
+	return (t.A+t.B) > t.C && (t.A+t.C) > t.B && (t.C+t.B) > t.A
+}
+
+func solution(context *cli.Context) (result int) {
+	var input = context.String("input")
+	data, err := parseInput(input)
+	if err != nil {
+		log.Error(fmt.Sprintf("Something went wrong while reading input file: %v", err))
+		return
+	}
+
+	for i, t := range data {
+		if isValid(t) {
+			log.Debug(fmt.Sprintf("Triangle (%d) %#v is VALID", i, t))
+			result++
+		} else {
+			log.Debug(fmt.Sprintf("Triangle (%d) %#v is INVALID", i, t))
+		}
+	}
+	return
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "AOC",
+		Usage: "Solve AdventOfCode problem!",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "loglevel",
+				Aliases: []string{"l"},
+				Value:   "info",
+				Usage:   "Log level output",
+			},
+			&cli.StringFlag{
+				Name:    "input",
+				Aliases: []string{"i"},
+				Value:   "input.txt",
+				Usage:   "Input file path",
+			},
+			&cli.StringFlag{
+				Name:    "output",
+				Aliases: []string{"o"},
+				Value:   "-",
+				Usage:   "Output file path, use - for stdout",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			setLogLevel(c.String("loglevel"))
+			log.Debug("Received parameters:")
+			log.Debug(fmt.Sprintf("Input file name:  %s", c.String("input")))
+			log.Debug(fmt.Sprintf("Output file name: %s", c.String("output")))
+			log.Debug(fmt.Sprintf("Log level:        %s", c.String("loglevel")))
+			echo(fmt.Sprintf("Solution is %v", solution(c)), c.String("output"))
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
