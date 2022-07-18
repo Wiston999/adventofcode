0ae713dc9e5d131e6cea3c0b0054c2de700a7df7
Victor Cabezas <vcabezas@tuenti.com>
2022-07-18 17:04:25 +0200

    Add day 9

---
+++ 2016/day09/day09-2.go
@@ -0,0 +1,128 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/urfave/cli/v2"
+)
+
+func setLogLevel(level string) {
+	switch strings.ToLower(level) {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.Fatal(fmt.Sprintf("Unkown log level %s", level))
+	}
+}
+
+func echo(msg, f string) {
+	var file *os.File
+	if f == "-" {
+		file = os.Stdout
+	} else {
+		var err error
+		file, err = os.OpenFile(f, os.O_RDWR|os.O_CREATE, 0644)
+		if err != nil {
+			log.Error(fmt.Sprintf("Error opening file %s for writing output: %v", f, err))
+			return
+		}
+		defer file.Close()
+	}
+	fmt.Fprintf(file, msg)
+}
+
+func parseInput(input string) (data string, err error) {
+	tmpData, err := os.ReadFile(input)
+	if err != nil {
+		log.Error(fmt.Sprintf("Error opening file %s for reading input: %v", input, err))
+		return
+	}
+	data = strings.TrimSpace(string(tmpData))
+	return
+}
+
+func decompress(s string) (result int) {
+	log.Debug(fmt.Sprintf("Decompressing %s", s))
+	for i := 0; i < len(s); {
+		c := s[i]
+		log.Debug(fmt.Sprintf("Pointer at %04d (%s) result is %d", i, string(c), result))
+		if string(c) == "(" {
+			end := strings.Index(s[i:], ")")
+			command := strings.Split(s[i+1:i+end], "x")
+			log.Debug(fmt.Sprintf("Splitted %s into: %#v", s[i+1:i+end], command))
+			c1, _ := strconv.Atoi(command[0])
+			c2, _ := strconv.Atoi(command[1])
+			result += decompress(s[i+end+1:i+end+c1+1]) * c2
+			i += end + c1 + 1
+		} else {
+			i++
+			result++
+		}
+		log.Debug(fmt.Sprintf("New pointer at %04d result is %d", i, result))
+	}
+	return
+}
+
+func solution(context *cli.Context) (result int) {
+	var input = context.String("input")
+	data, err := parseInput(input)
+	if err != nil {
+		log.Error(fmt.Sprintf("Something went wrong while reading input file: %v", err))
+		return
+	}
+
+	result = decompress(data)
+	return
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "AOC",
+		Usage: "Solve AdventOfCode problem!",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "loglevel",
+				Aliases: []string{"l"},
+				Value:   "info",
+				Usage:   "Log level output",
+			},
+			&cli.StringFlag{
+				Name:    "input",
+				Aliases: []string{"i"},
+				Value:   "input.txt",
+				Usage:   "Input file path",
+			},
+			&cli.StringFlag{
+				Name:    "output",
+				Aliases: []string{"o"},
+				Value:   "-",
+				Usage:   "Output file path, use - for stdout",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			setLogLevel(c.String("loglevel"))
+			log.Debug("Received parameters:")
+			log.Debug(fmt.Sprintf("Input file name:  %s", c.String("input")))
+			log.Debug(fmt.Sprintf("Output file name: %s", c.String("output")))
+			log.Debug(fmt.Sprintf("Log level:        %s", c.String("loglevel")))
+			echo(fmt.Sprintf("Solution is %v", solution(c)), c.String("output"))
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
