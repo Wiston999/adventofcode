96686c5775e4b7c56dfb1a5e9e9ed2ee3d0d6884
Victor Cabezas <vcabezas@tuenti.com>
2022-07-26 16:15:41 +0200

    Add day 17

---
+++ 2016/day15/day15-1.go
@@ -0,0 +1,151 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"regexp"
+	"strconv"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/urfave/cli/v2"
+)
+
+type Disc struct {
+	Name              int
+	Position, Current int
+}
+
+func (d *Disc) Step(i int) (s int) {
+	s = (d.Current + i) % d.Position
+	return
+}
+
+type Problem struct {
+	Discs map[int]Disc
+}
+
+func setLogLevel(level string) {
+	switch strings.ToLower(level) {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.Fatal(fmt.Sprintf("Unkown log level %s", level))
+	}
+}
+
+func echo(msg, f string) {
+	var file *os.File
+	if f == "-" {
+		file = os.Stdout
+	} else {
+		var err error
+		file, err = os.OpenFile(f, os.O_RDWR|os.O_CREATE, 0644)
+		if err != nil {
+			log.Error(fmt.Sprintf("Error opening file %s for writing output: %v", f, err))
+			return
+		}
+		defer file.Close()
+	}
+	fmt.Fprintf(file, msg)
+}
+
+func atoi(a string) (i int) {
+	i, _ = strconv.Atoi(a)
+	return
+}
+
+func parseInput(input string) (data Problem, err error) {
+	tmpData, err := os.ReadFile(input)
+	if err != nil {
+		log.Error(fmt.Sprintf("Error opening file %s for reading input: %v", input, err))
+		return
+	}
+	regex := *regexp.MustCompile(`(?m)^Disc #(\d+) has (\d+) positions; at time=0, it is at position (\d+)\.$`)
+	res := regex.FindAllStringSubmatch(string(tmpData), -1)
+	data.Discs = make(map[int]Disc)
+	for i := range res {
+		d := Disc{
+			Name:     atoi(res[i][1]),
+			Position: atoi(res[i][2]),
+			Current:  atoi(res[i][3]),
+		}
+		log.Debug(fmt.Sprintf("Parsed disk %#v", d))
+		data.Discs[d.Name] = d
+	}
+	return
+}
+
+func solution(context *cli.Context) (result int) {
+	var input = context.String("input")
+	data, err := parseInput(input)
+	if err != nil {
+		log.Error(fmt.Sprintf("Something went wrong while reading input file: %v", err))
+		return
+	}
+
+	for i := 0; ; i++ {
+		success := true
+		for k, d := range data.Discs {
+			if d.Step(i+k) != 0 {
+				success = false
+				break
+			}
+		}
+
+		if success {
+			result = i
+			break
+		}
+	}
+
+	return
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "AOC",
+		Usage: "Solve AdventOfCode problem!",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "loglevel",
+				Aliases: []string{"l"},
+				Value:   "info",
+				Usage:   "Log level output",
+			},
+			&cli.StringFlag{
+				Name:    "input",
+				Aliases: []string{"i"},
+				Value:   "input.txt",
+				Usage:   "Input file path",
+			},
+			&cli.StringFlag{
+				Name:    "output",
+				Aliases: []string{"o"},
+				Value:   "-",
+				Usage:   "Output file path, use - for stdout",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			setLogLevel(c.String("loglevel"))
+			log.Debug("Received parameters:")
+			log.Debug(fmt.Sprintf("Input file name:  %s", c.String("input")))
+			log.Debug(fmt.Sprintf("Output file name: %s", c.String("output")))
+			log.Debug(fmt.Sprintf("Log level:        %s", c.String("loglevel")))
+			echo(fmt.Sprintf("Solution is %v", solution(c)), c.String("output"))
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
