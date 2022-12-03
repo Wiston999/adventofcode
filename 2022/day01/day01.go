3c19f8c4c2de8d56a60621bb0d2c3b8d43225e0a
Victor Cabezas <vcabezas@tuenti.com>
2022-12-03 19:50:14 +0100

    Add day01 script and full day 3

---
+++ 2022/day01/day01.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/urfave/cli/v2"
+)
+
+type Elf struct {
+	Calories int
+}
+
+type Problem struct {
+	Elfs []Elf
+}
+
+func setLogLevel(level string) {
+	switch strings.ToLower(level) {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.Fatal(fmt.Sprintf("Unkown log level %s", level))
+	}
+}
+
+func echo(msg, f string) {
+	var file *os.File
+	if f == "-" {
+		file = os.Stdout
+	} else {
+		var err error
+		file, err = os.OpenFile(f, os.O_RDWR|os.O_CREATE, 0644)
+		if err != nil {
+			log.Error(fmt.Sprintf("Error opening file %s for writing output: %v", f, err))
+			return
+		}
+		defer file.Close()
+	}
+	fmt.Fprintf(file, msg)
+}
+
+func parseInput(input string) (data Problem, err error) {
+	byteData, err := os.ReadFile(input)
+	if err != nil {
+		log.Error(fmt.Sprintf("Error opening file %s for reading input: %v", input, err))
+		return
+	}
+	strData := string(byteData)
+	curElf := Elf{}
+	for i, l := range strings.Split(strings.TrimSpace(strData), "\n") {
+		log.Debug(fmt.Sprintf("Parsing line %03d: %s", i, l))
+		cal, err := strconv.Atoi(l)
+		if err != nil {
+			data.Elfs = append(data.Elfs, curElf)
+			curElf = Elf{}
+			continue
+		}
+		curElf.Calories += cal
+	}
+	data.Elfs = append(data.Elfs, curElf)
+	return
+}
+
+func solution(context *cli.Context) (result int) {
+	var input = context.String("input")
+	problem, err := parseInput(input)
+	if err != nil {
+		log.Error(fmt.Sprintf("Something went wrong while reading input file: %v", err))
+		return
+	}
+
+	log.Debug(fmt.Sprintf("Parsed problem %#v", problem))
+
+	prev := 9999999999
+	for i := 0; i < context.Int("top"); i++ {
+		cur := 0
+		for _, e := range problem.Elfs {
+			log.Debug(fmt.Sprintf("%02d Testing %d > %d < %d", i, cur, e.Calories, prev))
+			if e.Calories > cur && e.Calories < prev {
+				cur = e.Calories
+			}
+		}
+		log.Info(fmt.Sprintf("%02d Top Elf has %d calories", i, cur))
+		result += cur
+		prev = cur
+	}
+
+	return
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "AOC",
+		Usage: "Solve AdventOfCode problem!",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "loglevel",
+				Aliases: []string{"l"},
+				Value:   "info",
+				Usage:   "Log level output",
+			},
+			&cli.StringFlag{
+				Name:    "input",
+				Aliases: []string{"i"},
+				Value:   "input.txt",
+				Usage:   "Input file path",
+			},
+			&cli.StringFlag{
+				Name:    "output",
+				Aliases: []string{"o"},
+				Value:   "-",
+				Usage:   "Output file path, use - for stdout",
+			},
+			&cli.IntFlag{
+				Name:    "top",
+				Aliases: []string{"t"},
+				Value:   1,
+				Usage:   "Top Elves calories, use 3 for second part solution",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			setLogLevel(c.String("loglevel"))
+			log.Debug("Received parameters:")
+			log.Debug(fmt.Sprintf("Input file name:  %s", c.String("input")))
+			log.Debug(fmt.Sprintf("Output file name: %s", c.String("output")))
+			log.Debug(fmt.Sprintf("Log level:        %s", c.String("loglevel")))
+			echo(fmt.Sprintf("Solution is %v", solution(c)), c.String("output"))
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
