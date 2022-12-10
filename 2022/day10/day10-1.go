862f2db80888dcceba4c3f3e449864b6691fbcfd
Victor Cabezas <vcabezas@tuenti.com>
2022-12-10 13:19:46 +0100

    Add day 10

---
+++ 2022/day10/day10-1.go
@@ -0,0 +1,163 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/urfave/cli/v2"
+)
+
+const (
+	NOOP = iota
+	ADDX
+)
+
+var cycles = map[int]bool{
+	20:  true,
+	60:  true,
+	100: true,
+	140: true,
+	180: true,
+	220: true,
+}
+
+type Operation struct {
+	Kind, Value int
+}
+
+type Problem struct {
+	Program []Operation
+	X, PC   int
+}
+
+func (p *Problem) Run() (strength int) {
+	delay := false
+	p.X = 1
+	for i := 1; i <= 220; i++ {
+		if _, ok := cycles[i]; ok {
+			log.Debug(fmt.Sprintf("Cycle %03d: Adding %d to result %d", i, i*p.X, strength))
+			strength += i * p.X
+		}
+		op := p.Program[p.PC]
+		if delay {
+			p.PC++
+			delay = false
+			p.X += op.Value
+		} else if op.Kind == ADDX && !delay {
+			delay = true
+		} else {
+			p.PC++
+		}
+	}
+	return
+}
+
+func setLogLevel(level string) {
+	switch strings.ToLower(level) {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.Fatal(fmt.Sprintf("Unkown log level %s", level))
+	}
+}
+
+func echo(msg, f string) {
+	var file *os.File
+	if f == "-" {
+		file = os.Stdout
+	} else {
+		var err error
+		file, err = os.OpenFile(f, os.O_RDWR|os.O_CREATE, 0644)
+		if err != nil {
+			log.Error(fmt.Sprintf("Error opening file %s for writing output: %v", f, err))
+			return
+		}
+		defer file.Close()
+	}
+	fmt.Fprintf(file, msg)
+}
+
+func parseInput(input string) (data Problem, err error) {
+	byteData, err := os.ReadFile(input)
+	if err != nil {
+		log.Error(fmt.Sprintf("Error opening file %s for reading input: %v", input, err))
+		return
+	}
+	strData := string(byteData)
+	for i, l := range strings.Split(strings.TrimSpace(strData), "\n") {
+		log.Debug(fmt.Sprintf("Parsing line %03d: %s", i, l))
+		op := Operation{}
+		if strings.HasPrefix(l, "addx") {
+			op.Kind = ADDX
+			v, _ := strconv.Atoi(strings.Split(l, " ")[1])
+			op.Value = v
+		}
+		data.Program = append(data.Program, op)
+	}
+	return
+}
+
+func solution(context *cli.Context) (result int) {
+	var input = context.String("input")
+	problem, err := parseInput(input)
+	if err != nil {
+		log.Error(fmt.Sprintf("Something went wrong while reading input file: %v", err))
+		return
+	}
+
+	log.Debug(fmt.Sprintf("Parsed problem %#v", problem))
+	result = problem.Run()
+
+	return
+}
+
+func main() {
+	app := &cli.App{
+		Name:  "AOC",
+		Usage: "Solve AdventOfCode problem!",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:    "loglevel",
+				Aliases: []string{"l"},
+				Value:   "info",
+				Usage:   "Log level output",
+			},
+			&cli.StringFlag{
+				Name:    "input",
+				Aliases: []string{"i"},
+				Value:   "input.txt",
+				Usage:   "Input file path",
+			},
+			&cli.StringFlag{
+				Name:    "output",
+				Aliases: []string{"o"},
+				Value:   "-",
+				Usage:   "Output file path, use - for stdout",
+			},
+		},
+		Action: func(c *cli.Context) error {
+			setLogLevel(c.String("loglevel"))
+			log.Debug("Received parameters:")
+			log.Debug(fmt.Sprintf("Input file name:  %s", c.String("input")))
+			log.Debug(fmt.Sprintf("Output file name: %s", c.String("output")))
+			log.Debug(fmt.Sprintf("Log level:        %s", c.String("loglevel")))
+			echo(fmt.Sprintf("Solution is %v", solution(c)), c.String("output"))
+			return nil
+		},
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
