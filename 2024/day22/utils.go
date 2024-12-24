1dc3a6929da293efd051e2efeb9c44cc749a0381
Victor Cabezas <vcabezas@tuenti.com>
2024-12-24 11:06:15 +0100

    Add day 23

---
+++ 2024/day22/utils.go
@@ -0,0 +1,185 @@
+package main
+
+import (
+	"fmt"
+	"math"
+	"os"
+	"strconv"
+	"strings"
+
+	log "github.com/sirupsen/logrus"
+
+	"github.com/oleiade/lane/v2"
+
+	"github.com/urfave/cli/v2"
+)
+
+type void struct{}
+
+var null void
+
+type Problem struct {
+	Secrets []int
+}
+
+type Sequence struct {
+	A, B, C, D int
+}
+
+func (p *Problem) Evolve(input int) (result int) {
+	tmp1 := ((input * 64) ^ input) % 16777216
+	tmp2 := ((tmp1 / 32) ^ tmp1) % 16777216
+	result = ((tmp2 * 2048) ^ tmp2) % 16777216
+	return
+}
+
+func (p *Problem) Part1() (result int) {
+	for _, s := range p.Secrets {
+		for i := 0; i < 2000; i++ {
+			s = p.Evolve(s)
+		}
+		result += s
+	}
+	return
+}
+
+func (p *Problem) Part2() (result int) {
+	partials := make(map[Sequence]int)
+	for _, s := range p.Secrets {
+		secretDiff := make([]int, 0)
+		visited := make(map[Sequence]void)
+		for i := 0; i < 2000; i++ {
+			n := p.Evolve(s)
+			secretDiff = append(secretDiff, (n%10)-(s%10))
+			s = n
+			if i > 3 {
+				seq := Sequence{secretDiff[i-3], secretDiff[i-2], secretDiff[i-1], secretDiff[i]}
+				if _, ok := visited[seq]; !ok {
+					partials[seq] += n % 10
+				}
+				visited[seq] = null
+			}
+		}
+	}
+	for _, v := range partials {
+		if v > result {
+			result = v
+		}
+	}
+	return
+}
+
+func NewProblem(ctx *cli.Context) (p *Problem, err error) {
+	input := ctx.String("input")
+	byteData, err := os.ReadFile(input)
+	if err != nil {
+		log.Error(fmt.Sprintf("Error opening file %s for reading input: %v", input, err))
+		return
+	}
+	p = new(Problem)
+	strData := string(byteData)
+	for i, l := range strings.Split(strings.TrimSpace(strData), "\n") {
+		log.Debug(fmt.Sprintf("Parsing line %03d: %s", i, l))
+		p.Secrets = append(p.Secrets, atoi(l))
+	}
+	return
+}
+
+func setLogLevel(level string) {
+	switch strings.ToLower(level) {
+	case "debug":
+		log.SetLevel(log.DebugLevel)
+	case "info":
+		log.SetLevel(log.InfoLevel)
+	case "warn":
+		log.SetLevel(log.WarnLevel)
+	case "error":
+		log.SetLevel(log.ErrorLevel)
+	default:
+		log.Fatal(fmt.Sprintf("Unkown log level %s", level))
+	}
+}
+
+func atoi(a string) (i int) {
+	i, _ = strconv.Atoi(a)
+	return
+}
+
+func echo(msg, f string) {
+	var file *os.File
+	if f == "-" {
+		file = os.Stdout
+	} else {
+		var err error
+		file, err = os.OpenFile(f, os.O_RDWR|os.O_CREATE, 0644)
+		if err != nil {
+			log.Error(fmt.Sprintf("Error opening file %s for writing output: %v", f, err))
+			return
+		}
+		defer file.Close()
+	}
+	fmt.Fprintf(file, msg)
+}
+
+type State struct {
+	ID int
+}
+
+func (s *State) Neighbours(p Problem) (ns []State) {
+
+	return
+}
+
+type PathFinder struct {
+	P         Problem
+	Start     State
+	Goal      func(State) bool
+	Cost      func(State, State) float64
+	Heuristic func(State) float64
+}
+
+func (p *PathFinder) Search() (path []State, score float64) {
+	start := p.Start
+	pending := lane.NewMinPriorityQueue[State, float64]()
+	pending.Push(start, 0)
+
+	gScore := make(map[State]float64)
+	gScore[start] = 0
+
+	cameFrom := make(map[State]State)
+	cameFrom[start] = start
+
+	fScore := make(map[State]float64)
+	fScore[p.Start] = p.Heuristic(p.Start)
+	for pending.Size() > 0 {
+		current, _, _ := pending.Pop()
+		if p.Goal(current) {
+			log.Info(fmt.Sprintf("PathFinder Found solution %v", gScore[current]))
+			curr := current
+			for curr != start {
+				path = append(path, curr)
+				curr = cameFrom[curr]
+			}
+			return path, gScore[current]
+		}
+
+		for _, n := range current.Neighbours(p.P) {
+			tentative := gScore[current] + p.Cost(n, current)
+			if v, ok := gScore[n]; !ok || tentative < v {
+				gScore[n] = tentative
+				fScore[n] = tentative + p.Heuristic(n)
+				pending.Push(n, fScore[n])
+				cameFrom[n] = current
+			}
+		}
+	}
+	return
+}
+
+type Coord struct {
+	X, Y int
+}
+
+func (c *Coord) Manhattan(oc Coord) float64 {
+	return math.Abs(float64(oc.X-c.X)) + math.Abs(float64(oc.Y-c.Y))
+}
